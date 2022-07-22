84e9bf2b867593a8e0bc507cd60129306e2fd0a9
malfell <malfellows13@gmail.com>
2022-07-22 13:56:56 -0500

    added todo slice

--- src/features/todoSlice.js
+++ src/features/todoSlice.js
@@ -1,31 +1,41 @@
+// import createSlice
 import { createSlice } from '@reduxjs/toolkit'
 
+// establish initial state
 const initialState = {
     items: []
 }
 
+// to do slice
 export const todoSlice = createSlice({
     name: 'todos',
     initialState,
     reducers: {
+        // returns new item plus rest of array
         addTodo: (state, action) => {
             return { items: [...state.items, action.payload]}
         },
+        // removes one item
         removeOne: (state, action) => {
             console.log(action)
+            // variables
             let array = [...state.items]
             let index = action.payload
+            // splices chosen item out of array
             if(index !== -1) {
                 array.splice(index, 1)
                 return { items: array }
             }
         },
+        // returns empty array
         clearTodo: () => {
             return { items: [] }
         }
     }
 })
 
+// export actions
 export const { addTodo, removeOne, clearTodo } = todoSlice.actions
 
+// export reducer
 export default todoSlice.reducer
--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './features/counterSlice'
+// import to do reducer
 import todoReducer from './features/todoSlice'
 
 export const store = configureStore({
     reducer: {
         counter: counterReducer,
+        // add todo reducer
         todos: todoReducer
     }
 })
